2dc9f226ce7dbe9cd649345d1dcf1a1a1958e2dc
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-28 18:01:56 -0400

    Please.

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-var PlayerModel = require('./model/playermodel');
-var DirectionModel =require('./model/directionmodel')
-var DirectionView = require('./view/directionview');
-var PlayerView = require('./view/playerview');
+
+let MyRouter = require('./router');
 
 window.addEventListener('load', function(){
  console.log('I am functional');
-
- let vmodel = new PlayerModel();
-
- let vdirection = new DirectionModel();
-
-
- let player = new PlayerView({
-   model: vmodel,
-   el: document.getElementById('player-view'),
- });
-
- let direction = new DirectionView({
-   model: vdirection,
-   el: document.getElementById('direction-view')
- })
+var GameRouter = new MyRouter;
+Backbone.history.start()
 });
